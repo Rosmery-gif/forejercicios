b03a36a59a3b8db938e72bc6a5f8e144172421b1
Rosmery-gif <00020725@uca.edu.sv>
2025-05-20 12:47:36 -0600

    M. Campos 00020725 ejercicios FOR :apple: :bug:

--- nprimos.cpp
+++ nprimos.cpp
@@ -3,33 +3,28 @@ using namespace std;
 
 int main(){
 
-    int numero=0, primo=1, modulo=0;
+    int numero=0, primo=1, modulo;
 
     cout<<"ingrese un numero entero";
     cin>>numero;
 
-    for (int i=numero-1; i>=2; numero=numero-1)
+    for (int i=numero-1; i>=2; i=i-1)
     {
-        modulo= numero%1;
+        modulo = numero%i;
 
         if(modulo == 0){
             cout<<"no es primo";
             primo =0;
 
-             break;
+          break;   
         }
-
+    }
     
-       
-    }
-    if(primo == 1){
-        cout<<"Es primo";
-    }
-
+       if(primo == 1){
+        cout<<"es primo";
     
 
+}
 
-
-
-
+return 0;
 }
